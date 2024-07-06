13b161394d360ae7cf3aa04c0f089f1614f1aee8
anshv-py <119095215+anshv-py@users.noreply.github.com>
2024-07-06 20:15:39 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'C:/Users/anshv/OneDrive/Desktop/Ansh Personal/Inspired Singapore/WiKnow/wiknow/src/styles/Footer.css'
+import 'src/styles/Footer.css'
 
 const Footer = () => {
     return (
