6dbbec8051edf074fbd24009a9814c11d54c36d8
anshv-py <119095215+anshv-py@users.noreply.github.com>
2024-07-06 20:16:25 +0530

    Update ButtonGroup.js

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import TextArea from './TextArea';
 // import axios from 'axios';
-import 'C:/Users/anshv/OneDrive/Desktop/Ansh Personal/Inspired Singapore/WiKnow/wiknow/src/styles/ButtonGroup.css';
+import 'src/styles/ButtonGroup.css';
 
 const ButtonGroup = (props) => {
 
